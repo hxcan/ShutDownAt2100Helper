d608cde84eca30b2125ec5a8cb2554640e5054b3
Hxcan Cai <caihuosheng@gmail.com>
2022-09-29 09:15:40 +0800

    Do not launch my own StopUsingPhoneActivity

---
+++ autoshutdowntest/src/main/java/com/stupidbeauty/shutdownat2100androidnative/Constants.java
@@ -0,0 +1,23 @@
+package com.stupidbeauty.shutdownat2100androidnative;
+
+/**
+ * 一些常量的定义。
+ * @author root 蔡火胜。
+ *
+ */
+@SuppressWarnings("WeakerAccess")
+public class Constants
+{
+    /**
+     * 常用常量。
+     * @author root 蔡火胜。
+     *
+     */
+    public final class Common
+    {
+        public static final String SHUTDOWN_HOUR="shutdownHour"; //!<关机的小时数。
+		public static final String SHUTDOWN_MINUTE="shutdownMinute"; //!<关机的分钟数。
+
+    } //public final class Common
+
+} //public class Constants
---
+++ autoshutdowntest/src/main/java/com/stupidbeauty/shutdownat2100androidnative/PreferenceManagerUtil.java
@@ -0,0 +1,60 @@
+package com.stupidbeauty.shutdownat2100androidnative;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.stupidbeauty.lanime.QueueMateStoreApplication;
+
+@SuppressWarnings("WeakerAccess")
+public class PreferenceManagerUtil {
+
+	/**
+	 * 保存客户端编号。
+	 * @param clientId 要保存的客户端编号。
+	 */
+	public static void setShutdownHour(int clientId)
+	{
+		Context ct = QueueMateStoreApplication.getAppContext(); //获取应用程序上下文。
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(ct); //获取共享配置对象。
+		sp.edit().putInt(Constants.Common.SHUTDOWN_HOUR, clientId).apply(); //保存。
+	} //public static void set
+
+
+    /**
+     * 获取关机时间的小时数。
+     * @return 关机时间的小时数。
+     */
+    public static int getShutdownHour()
+    {
+        Context ct = QueueMateStoreApplication.getAppContext(); //获取应用程序上下文。
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(ct); //获取默认共享配置对象。
+        return sp.getInt(Constants.Common.SHUTDOWN_HOUR, 21);
+
+    } //public static String get
+
+	/**
+	 * 获取关机时间的分钟数。
+	 * @return 关机时间的分钟数。
+	 */
+	public static int getShutdownMinute()
+	{
+		Context ct = QueueMateStoreApplication.getAppContext(); //获取应用程序上下文。
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(ct); //获取默认共享配置对象。
+		return sp.getInt(Constants.Common.SHUTDOWN_MINUTE, 45);
+
+	} //public static String get
+
+
+	/**
+	 * 保存客户端编号。
+	 * @param clientId 要保存的客户端编号。
+	 */
+	public static void setShutdownMinute(int clientId)
+	{
+		Context ct = QueueMateStoreApplication.getAppContext(); //获取应用程序上下文。
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(ct); //获取共享配置对象。
+		sp.edit().putInt(Constants.Common.SHUTDOWN_MINUTE, clientId).apply(); //保存。
+	} //public static void set
+
+}
