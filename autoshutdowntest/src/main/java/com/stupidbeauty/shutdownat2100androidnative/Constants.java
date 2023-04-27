ad90ebc7b871d9dcab41a945c0d32c209d9ad1d9
Hxcan Cai <caihuosheng@gmail.com>
2023-04-27 18:41:01 +0800

    migrate to ShutDownAt2100Helper

--- autoshutdowntest/src/main/java/com/stupidbeauty/shutdownat2100androidnative/Constants.java
+++ autoshutdowntest/src/main/java/com/stupidbeauty/shutdownat2100androidnative/Constants.java
@@ -1,5 +1,8 @@
 package com.stupidbeauty.shutdownat2100androidnative;
 
+import android.os.Environment;
+import java.io.File;
+
 /**
  * 一些常量的定义。
  * @author root 蔡火胜。
@@ -19,5 +22,20 @@ public class Constants
 		public static final String SHUTDOWN_MINUTE="shutdownMinute"; //!<关机的分钟数。
 
     } //public final class Common
+
+    public final class Timing
+    {
+        public static final long MinimalPublishIntervalMilliseconds=1*1000; //!<最小的发布服务时间间隔。
+    }
+
+    /**
+     * 目录路径。
+     * @author root 蔡火胜。
+     *
+     */
+    public static class DirPath
+    {
+        public static final String FARMING_BOOK_APP_SD_CARD_PATH = Environment.getExternalStorageDirectory().getPath()+ File.separator+ "etc" + File.separator+"ShutDownAt2100"; //女神相机的路径。
+    } //public static class DirPath
 
 } //public class Constants
